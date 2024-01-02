f913b34a7f8a23fc3de03fcc1a717a21084698b5
Squareroot2-Bit <Squareroot_2@outlook.com>
2024-01-03 03:14:26 +0800

    Initial commit

---
+++ src/main/java/com/sqrt2/Main.java
@@ -0,0 +1,13 @@
+package com.sqrt2;
+
+/**
+ * @ClassName ${NAME}
+ * @Description
+ * @date 2024/1/3 3:11
+ * @Author Squareroot_2
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
